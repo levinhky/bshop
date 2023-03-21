61bb2e31420b2810df7e35da5980dd86b4998b5f
levinhky <chansolle@gmail.com>
2023-03-21 20:14:17 +0700

    up

--- js/Page Cart Success.js
+++ js/Page Cart Success.js
@@ -1,8 +1,24 @@
-$bankCopyBtns = $(".box-cart-success-container.banking-info .banking-method .manual-banking table tr td .bank-copy");
-$bankCopyContents = $(".box-cart-success-container.banking-info .banking-method .manual-banking table tr td .bank-copy-content");
+$bankCopyBtns = $(
+  ".box-cart-success-container.banking-info .banking-method .manual-banking table tr td .bank-copy"
+);
+$bankCopyContents = $(
+  ".box-cart-success-container.banking-info .banking-method .manual-banking table tr td .bank-copy-content"
+);
 
-$bankCopyBtns.each(function(index) {
-    $(this).on('click', function() {
-        console.log($(this).prev().find('.bank-copy-content'));
-    })
-})
+$bankCopyBtns.each(function (index) {
+  $(this).on("click", function () {
+    $value = $(this).closest("td").prev().find("p").text().replace(/\s+/g, "");
+    copyToClipboard($value);
+    $(this).parent().find("#copiedToolTip").addClass("show");
+    setTimeout(() =>   $(this).parent().find("#copiedToolTip").removeClass("show"), 1000);
+  });
+});
+
+function copyToClipboard(text) {
+  var sampleTextarea = document.createElement("textarea");
+  document.body.appendChild(sampleTextarea);
+  sampleTextarea.value = text;
+  sampleTextarea.select();
+  document.execCommand("copy");
+  document.body.removeChild(sampleTextarea);
+}
