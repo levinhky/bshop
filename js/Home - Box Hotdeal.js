470f9e8ebef04a6584d8c71f620abd142f6b859e
levinhky <chansolle@gmail.com>
2023-03-22 13:40:10 +0700

    Box Hotdeal

---
+++ js/Home - Box Hotdeal.js
@@ -0,0 +1,12 @@
+$('.box-hot-deal-content .product-listing').owlCarousel({
+    autoplay: false,
+    loop: true,
+    infinite:true,
+    nav: true,
+    dots:false,
+    navText:["<img src='./images/arrow-left.svg'>",
+    "<img src='./images/arrow-right.svg'>"],
+    items: 5,
+    margin: 8,
+  });
+  
