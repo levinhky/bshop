3fc6c10222258487351f88a882ea268f42d565a1
levinhky <chansolle@gmail.com>
2023-03-28 19:40:55 +0700

    update carousel

--- js/Home - Thương Hiệu Nổi Bật.js
+++ js/Home - Thương Hiệu Nổi Bật.js
@@ -1,13 +1,13 @@
-$nextArrow = $(".brand__listing .arrow-slider .arrow--right");
-$prevArrow = $(".brand__listing .arrow-slider .arrow--left");
-$brandCarousel = $(".brand__listing .brands .brand-carousel");
-
-$nextArrow.on("click", function () {
-  $lists = $(".brand__listing .brands .brand-carousel .brand");
-  $brandCarousel.append($lists.get(0));
-});
-
-$prevArrow.on("click", function () {
-  $lists = $(".brand__listing .brands .brand-carousel .brand");
-  $brandCarousel.prepend($lists[$lists.length -1])
+$(".owl-carousel").owlCarousel({
+  autoplay: false,
+  loop: true,
+  infinite: true,
+  nav: true,
+  dots: false,
+  navText: [
+    "<img src='./images/arrow-left.svg'>",
+    "<img src='./images/arrow-right.svg'>",
+  ],
+  items: 4,
+  margin: 50,
 });
